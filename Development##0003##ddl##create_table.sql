5a2cd25242f5ba9447c7857ddc1b9974236d4f24
putupradnya <putuandhika@hotmail.com>
2024-01-27 18:26:16 +0700

    new lecturer table

---
+++ Development##0003##ddl##create_table.sql
@@ -0,0 +1,8 @@
+CREATE table newLecturer(
+    PersonID int,
+    LastName varchar(255),
+    FirstName varchar(255),
+    Address varchar(255),
+    City varchar(255),
+    country varchar(255)
+);
