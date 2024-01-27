758701034f1addbea30140610d3f3c98dac712ed
putupradnya <putuandhika@hotmail.com>
2024-01-27 18:04:16 +0700

    country

--- bytebase/test/Development##0001##DDL##test_task.sql
+++ bytebase/test/Development##0001##DDL##test_task.sql
@@ -3,5 +3,6 @@ CREATE table detailBuyers(
     LastName varchar(255),
     FirstName varchar(255),
     Address varchar(255),
-    City varchar(255)
+    City varchar(255),
+    countryName varchar(255)
 );
